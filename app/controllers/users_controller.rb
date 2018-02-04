bbac23191bf441b97b5e2a163457f9c006093269
Kai Yong <dkang.kky@gmail.com>
2018-02-04 11:02:31 +1100

    add form for user to sign up, leave the select2 filter result first

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < Clearance::UsersController
+
+  def create
+    byebug
+    @user = user_from_params
+
+    respond_to do |format|
+      if @user.save
+        format.html {redirect_back_or url_after_create }
+        format.html { sign_in @user }
+      else
+        format.html { render template: "users/new"}
+      end
+    end
+
+  end
+
+  # def edit
+  #     @user = User.find(params[:id])
+  # end
+
+  # def update
+  #     @user = User.find(params[:id])
+  #     if @user.update(user_params)
+  #       redirect_to '/home/index'
+  #     else
+  #       render 'edit'
+  #     end
+  # end
+
+  private
+
+  # method from the gem 'clearance'
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+  
+  def user_from_params
+    User.new(user_params)
+  end
+end
