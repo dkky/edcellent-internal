4f21f927161ebffe84b3690f689833ed94b78626
Kai Yong <dkang.kky@gmail.com>
2018-04-03 14:54:23 +1000

    add new role: trial

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :attending_periods, through: :groups, source: :periods
   # belongs_to :group, optional: true
   has_one :profile
-  enum user_access: [:admin, :student, :tutor, :superadmin, :alumni, :dropout]
+  enum user_access: [:admin, :student, :tutor, :superadmin, :alumni, :dropout, :trial]
 
   filterrific(
     default_setting: { sorted_by: 'created_at_desc' },
